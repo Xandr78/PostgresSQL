5dd081d7be59c3c340176430d564b2f593dfc859
Alexandr Tushnov <xandr7891@inbox.ru>
2023-05-08 20:56:08 +0300

    change CORRECTIONS

--- music_CREATE.sql
+++ music_CREATE.sql
@@ -1,3 +1,19 @@
+
+--drop table track_collection;
+
+--drop table track;  
+
+--drop table album_executor;
+
+--drop table album;
+
+--drop table genre_executor;
+
+--drop table executor;
+
+--drop table genre;
+
+
 --жанры
 create table if not exists genre (
 	id serial primary key,
@@ -14,14 +30,14 @@ create table if not exists executor (
 create table if not exists album (
 	id serial primary key,
 	name_album varchar(20) not null,
-	date_album varchar(10) not null
+	date_album integer not null
 );
 
 --треки
 create table if not exists track (
 	id serial primary key,
 	name_track varchar(40) not null,
-	duration_track varchar(20) not null,
+	duration_track integer not null,
 	album_id integer not null references album(id)
 );
 
@@ -29,7 +45,7 @@ create table if not exists track (
 create table if not exists collection (
 	id serial primary key,
 	name_collection varchar(20) not null,
-	date_collection varchar(10) not null
+	date_collection integer not null
 );
 
 --жанр-исполнитель(промежуточная таблица)
@@ -53,8 +69,8 @@ create table if not exists track_collection(
 	constraint pk_tc primary key (track_id, collection_id)
 );
 
-alter table album alter column date_album type integer using (date_album::integer);
+--alter table album alter column date_album type integer using (date_album::integer);
 
-alter table track alter column duration_track type integer using (duration_track::integer);
+--alter table track alter column duration_track type integer using (duration_track::integer);
 
-alter table collection alter column date_collection type integer using (date_collection::integer);
+--alter table collection alter column date_collection type integer using (date_collection::integer);
--- music_INSERT.sql
+++ music_INSERT.sql
@@ -1,4 +1,4 @@
-insert  into genre(name_genre)
+ insert  into genre(name_genre)
 values ('Рок'),
 	('Поп'),
 	('Соул'),
@@ -18,53 +18,53 @@ values ('Linkin Park'),
 ;
 
 insert into album (name_album, date_album)
-values ('альбом 1', 2000),
+values ('альбом 1', 2019),
 	('альбом 2', 1997),
-	('альбом 3', 2015),
-	('альбом 4', 2000),
+	('альбом 3', 2020),
+	('альбом 4', 2018),
 	('альбом 5', 2011)
 ;
 
-UPDATE album 
-SET name_album = 'альбом 4', date_album = 2018
-WHERE id = 13;
+--UPDATE album 
+--SET name_album = 'альбом 4', date_album = 2018
+--WHERE id = 13;
 
-UPDATE album 
-SET name_album = 'альбом 1', date_album = 2019
-WHERE id = 10;
+--UPDATE album 
+--SET name_album = 'альбом 1', date_album = 2019
+--WHERE id = 10;
 
-update album
-set name_album = 'альбом 3', date_album =2020
-where id = 12;
+--update album
+--set name_album = 'альбом 3', date_album =2020
+--where id = 12;
 
-UPDATE track 
-set   name_track = 'trackmy1', duration_track = 6, album_id = 10
-WHERE id = 1;
+--UPDATE track 
+--   name_track = 'trackmy1', duration_track = 6, album_id = 10
+--WHERE id = 1;
 
-update album_executor
-set album_id = 13, executor_id = 8
-where album_id = 13 and executor_id = 7
+--update album_executor
+--set album_id = 13, executor_id = 8
+--where album_id = 13 and executor_id = 7
 
 insert  into track (name_track, duration_track, album_id)
-values ('track_1', 3, 10),
-	('track_2', 2, 10),
-	('track_3', 3, 11),
-	('track_4', 6, 11),
-	('track_5', 5, 11),
-	('track_6', 2, 12),
-	('track_7', 3, 12),
-	('track_8', 4, 12),
-	('track_9', 3.5, 12),
-	('track_10', 5, 13),
-	('track_11', 3, 13),
-	('track_12', 2, 13),
-	('track_13', 4, 13),
-	('track_14', 1, 13),
-	('track_15', 3, 14)
+values ('track_new', 3, 5);
+
+insert  into track (name_track, duration_track, album_id)
+values ('trackmy1', 6, 1),
+	('track_2', 2, 1),
+	('track_3', 3, 2),
+	('track_4', 6, 2),
+	('track_5', 5, 2),
+	('track_6', 2, 3),
+	('track_7', 3, 3),
+	('track_8', 4, 3),
+	('track_9', 3.5, 3),
+	('track_10', 5, 4),
+	('track_11', 3, 4),
+	('track_12', 2, 4),
+	('track_13', 4, 4),
+	('track_14', 1, 4),
+	('trackмой15', 3, 5)
 ;
-
-insert  into track (name_track, duration_track, album_id)
-values ('track_new', 3, 14);
 
 insert into collection (name_collection, date_collection)
 values ('сборник 1', 2001),
@@ -78,35 +78,35 @@ values ('сборник 1', 2001),
 ;
 
 insert into genre_executor (genre_id, executor_id)
-values (3, 4),
+values (1, 1),
+	(1, 2),
+	(1, 4),
+	(2, 6),
+	(2, 8),
+	(5, 7),
+	(4, 3),
 	(3, 5),
 	(3, 7),
-	(4, 9),
-	(4, 11),
-	(7, 10),
-	(6, 6),
-	(5, 8),
-	(5, 10),
-	(3, 11),
-	(6, 11)
+	(1, 8),
+	(4, 8)
 ;
 
 insert into album_executor (album_id, executor_id)
-values (10, 4),
-	(10, 10),
-	(10, 11),
-	(11, 4),
-	(11, 5),
-	(11, 6),
-	(12, 4),
-	(12, 7),
-	(12, 11),
-	(13, 4),
-	(13, 7),
-	(13, 10),
-	(14, 8),
-	(14, 9),
-	(14, 11)
+values (1, 1),
+	(1, 7),
+	(1, 8),
+	(2, 1),
+	(2, 2),
+	(2, 3),
+	(3, 1),
+	(3, 4),
+	(3, 8),
+	(4, 1),
+	(4, 5),
+	(4, 7),
+	(5, 5),
+	(5, 6),
+	(5, 8)
 
 ;
 
