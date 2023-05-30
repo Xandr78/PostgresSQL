b19fdfaee56ac3739b2806969e0d6b2f43999951
Alexandr Tushnov <xandr7891@inbox.ru>
2023-05-30 05:57:13 +0300

    change sqlAlchemy2

--- DZ_sqlAlchemy_Class.py
+++ DZ_sqlAlchemy_Class.py
@@ -9,24 +9,26 @@ class Publisher(Base):
     __tablename__ = 'publisher'
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String, unique=True)
-    publisher_connect = relationship('Book', back_populates="books_connect")
+    publisher_book = relationship('Book', back_populates="books_publisher")
     def __str__(self):
         return f'publisher {self.id}: {self.name}'
 
 class Book(Base):
     __tablename__ = 'book'
     id = sq.Column(sq.Integer, primary_key=True)
-    title = sq.Column(sq.String)
-    id_published = sq.Column(sq.Integer, sq.ForeignKey('published.id'), nullable=False)
-    # publisher = relationship(Publisher, backref = 'book')
-    books_connect = relationship('Publisher', back_populates= "published_connect")
+    title = sq.Column(sq.String(length=40), unique=True)
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey('publisher.id'), nullable=False)
+    # publisher = relationship('Publisher', backref = 'book')
+    books_publisher = relationship('Publisher', back_populates= "publisher_book")
+    book_stock = relationship('Stock', back_populates= "stock_book")
     def __str__(self):
-        return f'book {self.id}: {self.title}, {self.id_published}'
+        return f'book {self.id}: {self.title}, {self.id_publisher}'
 #
 class Shop(Base):
     __tablename__ = 'shop'
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String)
+    shop_stock = relationship('Stock', back_populates= "stock_shop")
     def __str__(self):
         return f'shop {self.id}: {self.name}'
 
@@ -36,19 +38,23 @@ class Stock(Base):
     count = sq.Column(sq.Integer)
     id_book = sq.Column(sq.Integer, sq.ForeignKey('book.id'), nullable=False)
     id_shop = sq.Column(sq.Integer, sq.ForeignKey('shop.id'), nullable=False)
-    book = relationship(Book, backref = 'stock')
-    shop = relationship(Shop, backref = 'stock')
+    stock_shop = relationship('Shop', back_populates= "shop_stock")
+    stock_book = relationship('Book', back_populates= "book_stock")
+    stock_sale = relationship('Sale', back_populates= "sale_stock")
+    # stock_book = relationship('Book', backref = 'stock')
+    # stock_shop = relationship('Shop', backref = 'stock2')
     def __str__(self):
         return f'stock {self.id}: {self.count}, {self.id_book}, {self.id_shop}'
 
 class Sale(Base):
     __tablename__ = 'sale'
     id = sq.Column(sq.Integer, primary_key=True)
-    price = sq.Column(sq.Integer)
-    date_sale = sq.Column(sq.Integer)
+    price = sq.Column(sq.Float)
+    date_sale = sq.Column(sq.Date)
     count = sq.Column(sq.Integer)
     id_stock = sq.Column(sq.Integer, sq.ForeignKey('stock.id'), nullable=False)
-    stock = relationship(Stock, backref = 'sale')
+    sale_stock = relationship('Stock', back_populates= "stock_sale")
+    # sale_stock = relationship('Stock', backref = 'sale')
     def __str__(self):
         return f'sale {self.id}: {self.price}, {self.date_sale}, {self.count}, {self.id_stock}'
 
@@ -57,12 +63,8 @@ def create_tables(engine):
     Base.metadata.create_all(engine)
 
 
-# DSN = 'postgresql://postgres:laima@localhost:5432/books'
-# engine = sqlalchemy.create_engine(DSN)
-# create_tables(engine)
-# Session = sessionmaker(bind = engine)
-# session = Session()
 
 
 
-session.close()
+
+
--- DZ_sqlAlchemy_Work.py
+++ DZ_sqlAlchemy_Work.py
@@ -13,9 +13,90 @@ Session = sessionmaker(bind = engine)
 session = Session()
 #
 
-# with open("tests_data.json")  as f:
-#     data = json.load(f)
+with open("tests_data.json")  as f:
+    data = json.load(f)
 #     pprint(data)
 #
-#
+for record in data:
+    model = {
+        'publisher': Publisher,
+        'shop': Shop,
+        'book': Book,
+        'stock': Stock,
+        'sale': Sale,
+    }[record.get('model')]
+    session.add(model(id=record.get('pk'), **record.get('fields')))
+    # session.add(model(id=record['pk'], **record.get('fields')))
+session.commit()
+
+session.query()
+
+#РАБОТАЕТ!
+def get_shops(s):
+    if s.isdigit():
+        for tb, ns, ps, ds in session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).join(Publisher).join(Stock).join(Shop).join(Sale).filter(Publisher.id == s).all():
+            print(f"{tb:<40} {ns:<10} {ps:<8} {ds}")
+    else:
+        for tb, ns, ps, ds in session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).join(Publisher).join(Stock).join(Shop).join(Sale).filter(Publisher.name == s).all():
+            print(f"{tb:<40} {ns:<10} {ps:<8} {ds}")
+
+#НЕ РАБОТАЕТ!
+# def get_shops(s):
+#     if s.isdigit():
+#         for tb, ns, ps, ds in session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).join(Book.books_publisher).join(Stock.stock_book).join(Sale.sale_stock).join(Shop.shop_stock).filter(Publisher.id == s).all():
+#             print(f"{tb:<40} {ns:<10} {ps:<8} {ds}")
+#     else:
+#         for tb, ns, ps, ds in session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).join(Publisher).join(Stock).join(Shop).join(Sale).filter(Publisher.name == s).all():
+#             print(f"{tb:<40} {ns:<10} {ps:<8} {ds}")
+
+
+if __name__ == '__main__':
+   s = input("Введите имя или айди публициста: ", )
+   get_shops(s)
+
 session.close()
+
+
+
+
+
+
+
+
+
+
+#РАБОТАЕТ!
+# s = input("Введите имя или айди публициста: ", )
+# if s.isnumeric():
+#     for p in session.query(Publisher).filter(Publisher.id == s).all():
+#         print(p)
+# else:
+#     for p in session.query(Publisher).filter(Publisher.name == s).all():
+#         print(p)
+
+#РАБОТАЕТ!
+# s = input("Введите имя или айди публициста: ", )
+# if s.isdigit():
+#     for p in session.query(Publisher).filter(Publisher.id == s).all():
+#         print(p)
+# else:
+#     for p in session.query(Publisher).filter(Publisher.name == s).all():
+#         print(p)
+
+# for p in session.query(Publisher).filter(Publisher.name == "Pearson").all():
+#     print(p)
+# for p in session.query(Publisher).filter(Publisher.id == search()).all():
+#     print(p)
+# for b in session.query(Book).join(Book.books_connect).filter(Book.title.like('Nat%')).all():
+#     print(b)
+
+# #Вывод даных РАБОТАЕТ!
+# s = input("Введите имя или айди публициста: ", )
+# if s.isdigit():
+#     for ses in session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).join(Publisher).join(Stock).join(Shop).join(Sale).filter(Publisher.id == s).all():
+#         # print(str(ses[0]) + str(ses[1]) + str(ses[2]) + str(ses[3]))
+#         print(ses)
+# else:
+#     for ses in session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).join(Publisher).join(Stock).join(Shop).join(Sale).filter(Publisher.name == s).all():
+#         # print(str(ses[0]) + str(ses[1]) + str(ses[2]) + str(ses[3]))
+#         print(ses)
